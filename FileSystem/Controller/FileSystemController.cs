f0dc000d6b2f124b624cc34affb32abce09b090f
David Torrubia <davidtorrubiasantos@gmail.com>
2023-11-28 18:13:56 +0100

    FileNameVerification

--- FileSystem/Controller/FileSystemController.cs
+++ FileSystem/Controller/FileSystemController.cs
@@ -10,9 +10,25 @@ namespace FileSystem.FileSystemController
             string route;
             int size;
 
-            name = Menu.RequestStream<string>("Type the file name and extension:");
+            name = IsFileName();
             route = Menu.RequestStream<string>("Type the desired route:");
             size = Menu.RequestStream<int>("Type the file cluster size:");
+        }
+
+        private static string IsFileName (){
+            while (true)
+            {
+                var input = Menu.RequestStream<string>("Type the file name and extension:");
+                string[] stringParts = input.Split('.');
+                if (input != null && input.Trim() != "" && stringParts.Length >= 2)
+                {
+                    return input;
+                }
+                else
+                {
+                    Menu.Write("Incorrect file name or extension, you must input a name and extension", ColorEnum.Error);
+                }
+            }
         }
     }
 }
