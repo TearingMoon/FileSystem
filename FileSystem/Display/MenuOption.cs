ed696f270b7e6e5a3a9ca5e7cc2a4f2e006b8e15
David Torrubia <davidtorrubiasantos@gmail.com>
2023-11-28 12:53:06 +0100

    Namespace Changes

--- FileSystem/Display/MenuOption.cs
+++ FileSystem/Display/MenuOption.cs
@@ -1,13 +1,17 @@
-class MenuOption
+namespace FileSystem.Display
 {
-    public ConsoleColor BackgroundColor;
-    public ConsoleColor TextColor;
-    public string Text;
-
-    public MenuOption(string text, ConsoleColor backgrounColor = ConsoleColor.White, ConsoleColor textColor = ConsoleColor.Black)
+    class MenuOption
     {
-        Text = text;
-        BackgroundColor = backgrounColor;
-        TextColor = textColor;
+        public ConsoleColor BackgroundColor;
+        public ConsoleColor TextColor;
+        public string Text;
+
+        public MenuOption(string text, ConsoleColor backgrounColor = ConsoleColor.White, ConsoleColor textColor = ConsoleColor.Black)
+        {
+            Text = text;
+            BackgroundColor = backgrounColor;
+            TextColor = textColor;
+        }
+
     }
 }
